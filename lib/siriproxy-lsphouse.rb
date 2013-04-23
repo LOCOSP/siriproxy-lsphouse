c372248f8a97acad27fbc0f9a41f7c3dcaf5a2bb
LOCOSP <lokacyjny@gmail.com>
2013-04-23 04:01:52 +0300

    Update siriproxy-lsphouse.rb

--- lib/siriproxy-lsphouse.rb
+++ lib/siriproxy-lsphouse.rb
@@ -11,11 +11,31 @@ class SiriProxy::Plugin::RPi < SiriProxy::Plugin
 	request_completed
   end
   
+    listen_for (/turn (on|off) gallery/i) do |command|
+	command_gallery(command.downcase.strip)
+	request_completed
+  end
+  
+    listen_for (/turn (on|off) bed light/i) do |command|
+	command_bed_light(command.downcase.strip)
+	request_completed
+  end
+  
 ############# Actions
 
   def command_sidelight(command)
 	`tdtool --#{command} 2`
 	say "Sidelight is now turned #{command.capitalize}."
+  end
+  
+    def command_gallery(command)
+	`tdtool --#{command} 2`
+	say "Gallery is now turned #{command.capitalize}."
+  end
+  
+    def command_bed_light(command)
+	`tdtool --#{command} 2`
+	say "Bed light is now turned #{command.capitalize}."
   end
   
 ############# Initialization
